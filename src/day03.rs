748ff168e21a558a7a48d1a37eee237880facf5a
Tyler Oalman <tyleroalman@gmail.com>
2019-12-22 23:53:17 -0600

    Initialize day 3

---
+++ src/day03.rs
@@ -0,0 +1,18 @@
+use crate::error::Error;
+
+pub fn run<R>(input: R) -> Result<(), Error>
+where
+    R: std::io::BufRead,
+{
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_day03() {
+        unimplemented!()
+    }
+}
