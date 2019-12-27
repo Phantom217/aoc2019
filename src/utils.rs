5199426363cc8f26bf8bdb3fa953a70bbad92571
Tyler Oalman <tyleroalman@gmail.com>
2019-12-27 01:46:13 -0600

    Day 10

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,55 @@
+use crate::error::Error;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub(crate) struct F64(f64);
+
+impl std::convert::TryFrom<f64> for F64 {
+    type Error = Error;
+
+    fn try_from(f: f64) -> Result<Self, Self::Error> {
+        if f.is_nan() {
+            bail!("Cannot convert {} into F64.", f);
+        }
+
+        Ok(F64(f))
+    }
+}
+impl std::ops::Deref for F64 {
+    type Target = f64;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Eq for F64 {}
+
+impl std::hash::Hash for F64 {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: std::hash::Hasher,
+    {
+        self.0.to_bits().hash(state);
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub(crate) struct Point(i64, i64);
+
+impl Point {
+    pub(crate) fn new(x: i64, y: i64) -> Self {
+        Self(x, y)
+    }
+
+    pub(crate) fn x(&self) -> i64 {
+        self.0
+    }
+
+    pub(crate) fn y(&self) -> i64 {
+        self.1
+    }
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
     use crate::error::Error;
